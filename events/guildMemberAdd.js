eb597e07956c2dd01a1e309c8b17d3165f2fb98a
dani <daniellambrecht10@gmail.com>
2022-02-23 16:01:23 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,9 +4,10 @@ module.exports = {
 
 	name: 'guildMemberAdd',
 	
-	/* Emitted whenever a user joins a guild.
-	PARAMETER     TYPE               DESCRIPTION
-	member        GuildMember        The member that has joined a guild */
+	/**
+	 * emitted when a new user joins a guild 
+	 * @param {GuildMember} member 
+	 */
 	execute(member) {
         console.log(`New User "${member.user.username}" has joined "${member.guild.name}"`);
 		try {
