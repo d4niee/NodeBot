6fb34b5c0903df23a4b0f5ef31cf8a3d52f92e34
dani <daniellambrecht10@gmail.com>
2022-02-23 15:59:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -40,23 +40,6 @@ const utilities = new Utilities(client)
 utilities.setupCommands() // setting up the commands first
 utilities.setupEvents()   // adding and init events
 
-/* gets called if a command was send 
-PARAMETER     TYPE          DESCRIPTION
-interaction   Interaction   handle the interaction */
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return
-  const command = client.commands.get(interaction.commandName)
-  if (!command) return
-  try {
-    // run command if the command is valid
-    await command.execute(interaction)
-    //utilities.commandLog(interaction)
-  } catch (error) {
-    console.error(error)
-    await interaction.reply({ content: 'There was an error while executing this command!',
-      ephemeral: true })
-  }
-});
 /* distube events */
 const status = queue =>
   `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.join(', ') || 'Off'}\` | Loop: \`${
